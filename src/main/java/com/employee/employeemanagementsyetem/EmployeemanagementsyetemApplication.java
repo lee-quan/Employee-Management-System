5c09d64bd698184a54d5297ab271bc1f66ef5b2a
Lee Quan <82214819+lee-quan@users.noreply.github.com>
2022-12-31 12:46:40 +0800

    first commit

---
+++ src/main/java/com/employee/employeemanagementsyetem/EmployeemanagementsyetemApplication.java
@@ -0,0 +1,13 @@
+package com.employee.employeemanagementsyetem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementsyetemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagementsyetemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/employeemanagementsyetem/EmployeemanagementsyetemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.employeemanagementsyetem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementsyetemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
