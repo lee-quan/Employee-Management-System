3a4c2f5982c8b0d50ebe15d85bf945d6bcba320d
Lee Quan <82214819+lee-quan@users.noreply.github.com>
2023-01-03 18:13:09 +0800

    first commit

--- src/main/java/com/cbse/employeemanagementsyetem/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cbse.employeemanagementsyetem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cbse.employeemanagementsyetem.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-    
-}
