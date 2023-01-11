7f1b49db85dc9b7b14e369e2d13f5a101e08c75f
Lee Quan <leequan@Lees-MacBook-Pro.local>
2023-01-11 14:19:32 +0800

    test

--- src/main/java/com/springboot/ems/controller/MyUserDetails.java
+++ src/main/java/com/springboot/ems/controller/MyUserDetails.java
@@ -15,7 +15,9 @@ import com.springboot.ems.model.User;
 public class MyUserDetails implements UserDetails {
 
 	private User user;
-		
+	
+	private int test;
+	
 	public MyUserDetails(User user) {
 		this.user = user;
 	}
